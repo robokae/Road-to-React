b6fe7687ab063eadd1bfbe74f1110ead42ee98bc
robokae <alexanderhom19@gmail.com>
2022-05-20 17:23:41 -0700

    upload tutorial files

---
+++ src/App.js
@@ -0,0 +1,76 @@
+import React, { useState } from 'react';
+
+const App = () => {
+  const stories = [
+    {
+      title: 'React',
+      url: 'https://reactjs.org/',
+      author: 'Jordan Walke',
+      num_comments: 3,
+      points: 4,
+      objectID: 0,
+    },
+    {
+      title: 'Redux',
+      url: 'https://redux.js.org/',
+      author: 'Dan Abramov, Andrew Clark',
+      num_comments: 2,
+      points: 5,
+      objectID: 1,
+    },
+  ];
+
+  const [searchTerm, setSearchTerm] = useState('');
+
+  const handleSearch = (event) => {
+    setSearchTerm(event.target.value);
+  };
+
+  return (
+    <div>
+      <h1>My Hacker Stories</h1>
+
+      <Search onSearch={handleSearch} searchTerm={searchTerm}/>
+
+      <hr />
+
+      <List list={stories} />
+    </div>
+  )
+};
+
+const Search = (props) => {
+  return (
+    <div>
+      <label htmlFor="search">Search: </label>
+      <input id="search" type="text" onChange={props.onSearch} />
+
+      <p>
+        Searching for <strong>{props.searchTerm}</strong>
+      </p>
+    </div>
+  );
+};
+
+const List = (props) => {
+  return (
+    <ul>
+      {props.list.map((item) => (
+        <Item key={props.list.objectID} item={item} />
+      ))}
+    </ul>
+  );
+}
+
+const Item = (props) => (
+  <li>
+    <span>
+      <a href={props.item.url}>{props.item.title}</a>
+    </span>
+    <span>{props.item.author}</span>
+    <span>{props.item.num_comments}</span>
+    <span>{props.item.points}</span>
+  </li>
+);
+
+export default App;
