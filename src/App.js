1442697b0005718c2c9c06c61f0db4c845d3bf2c
robokae <alexanderhom19@gmail.com>
2022-05-23 12:59:43 -0700

    perform refactoring and implement search feature

--- src/App.js
+++ src/App.js
@@ -26,51 +26,76 @@ const App = () => {
     setSearchTerm(event.target.value);
   };
 
+  const searchedStories = stories.filter((story) => 
+    story.title.toLowerCase().includes(searchTerm.toLowerCase()) 
+  );
+
   return (
     <div>
       <h1>My Hacker Stories</h1>
 
-      <Search onSearch={handleSearch} searchTerm={searchTerm}/>
+      <Search onSearch={handleSearch} search={searchTerm}/>
 
       <hr />
 
-      <List list={stories} />
+      <List list={searchedStories} />
     </div>
   )
 };
 
-const Search = (props) => {
+const Search = ({ search, onSearch }) => {
+
   return (
     <div>
       <label htmlFor="search">Search: </label>
-      <input id="search" type="text" onChange={props.onSearch} />
-
-      <p>
-        Searching for <strong>{props.searchTerm}</strong>
-      </p>
+      <input 
+        id="search" 
+        type="text" 
+        value={search}
+        onChange={onSearch} 
+      />
     </div>
   );
 };
 
-const List = (props) => {
-  return (
-    <ul>
-      {props.list.map((item) => (
-        <Item key={props.list.objectID} item={item} />
-      ))}
-    </ul>
-  );
-}
+const List = ({ list }) => (
+  <ul>
+    {list.map((item) => (
+      <Item key={item.objectID} item={item} />
+    ))}
+  </ul>
+);
 
-const Item = (props) => (
+// alternative more concise version using the spread and rest operators
+// const List = ({ list }) => (
+//   <ul>
+//     {list.map(({ objectID, ...item }) => (
+//       <Item key={objectID} {...item} />
+//     ))}
+//   </ul>  
+// );
+
+const Item = ({ item }) => (
   <li>
     <span>
-      <a href={props.item.url}>{props.item.title}</a>
+      <a href={item.url}>{item.title}</a>
     </span>
-    <span>{props.item.author}</span>
-    <span>{props.item.num_comments}</span>
-    <span>{props.item.points}</span>
+    <span>{item.author}</span>
+    <span>{item.num_comments}</span>
+    <span>{item.points}</span>
   </li>
 );
+
+// alternative more concise version using object destructuring
+// const Item = ({ title, url, author, num_comments, points }) => (
+//   <li>
+//     <span>
+//       <a href={url}>{title}</a>
+//     </span>
+//     <span>{author}</span>
+//     <span>{num_comments}</span>
+//     <span>{points}</span>
+//   </li>
+// );
 
 export default App;
