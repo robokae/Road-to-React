73d4a8e8a86d27154f00dd06c6cbf073a2c4f3ab
robokae <alexanderhom19@gmail.com>
2022-06-07 15:32:08 -0700

    convert search field to form

--- src/App.js
+++ src/App.js
@@ -110,33 +110,25 @@ const App = () => {
     setSearchTerm(event.target.value);
   };
 
-  const handleSearchSubmit = () => {
+  const handleSearchSubmit = (event ) => {
     // update the endpoint URL
     setUrl(`${API_ENDPOINT}${searchTerm}`);
+
+    // prevent the browser from reloading
+    event.preventDefault();
   };
 
   return (
     <div>
       <h1>My Hacker Stories</h1>
 
-      <InputWithLabel
-        id="search"
-        value={searchTerm}
-        isFocused
-        onInputChange={handleSearchInput}
-      >
-        <strong>Search:</strong>
-      </InputWithLabel>
+        <SearchForm
+          searchTerm={searchTerm}
+          onSearchInput={handleSearchInput}
+          onSearchSubmit={handleSearchSubmit}
+        />
 
-      <button
-        type="button"
-        disabled={!searchTerm}
-        onClick={handleSearchSubmit}
-      >
-        Submit
-      </button>
-
-      <hr />
+        <hr />
 
       {stories.isError && <p>Something went wrong...</p>}
 
@@ -155,6 +147,30 @@ const App = () => {
     </div>
   )
 };
+
+const SearchForm = ({
+  searchTerm,
+  onSearchInput,
+  onSearchSubmit,
+}) => (
+  <form onSubmit={onSearchSubmit}>
+    <InputWithLabel
+      id="search"    
+      value={searchTerm}
+      isFocused
+      onInputChange={onSearchInput}
+    >
+      <strong>Search:</strong>
+    </InputWithLabel>
+
+    <button 
+      type="submit"
+      disabled={!searchTerm}
+    >
+      Submit
+    </button>
+  </form>
+);
 
 const InputWithLabel = ({
   id,
